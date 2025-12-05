7363e77f2ee453bb440594667e5560f6ebf0c0bf
abdellah alioua <abdellahalioua200@gmail.com>
2025-12-05 11:10:57 +0100

    Update bookmarks.js

--- functions/bookmarks.js
+++ functions/bookmarks.js
@@ -3,27 +3,37 @@ const { MongoClient } = require('mongodb');
 const client = new MongoClient(process.env.MONGODB_URI);
 
 exports.handler = async (event) => {
-    // Connect DB (Atlas)
-    if (!client.topology || !client.topology.isConnected()) await client.connect();
-    const collection = client.db('my_site_db').collection('bookmarks');
-
+    // 1. SETUP HEADERS to allow your other site to talk to this one
     const headers = {
-        'Access-Control-Allow-Origin': '*', // Allows your snippet to access this
-        'Access-Control-Allow-Methods': 'GET, POST'
+        'Access-Control-Allow-Origin': '*', // Allows requests from couponscorpion.com
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
     };
 
+    // 2. Handle PREFLIGHT requests (Browser security check)
+    if (event.httpMethod === 'OPTIONS') {
+        return { statusCode: 200, headers, body: '' };
+    }
+
     try {
+        if (!client.topology || !client.topology.isConnected()) await client.connect();
+        const collection = client.db('my_site_db').collection('bookmarks');
+
         if (event.httpMethod === 'POST') {
-            // Save new
             const data = JSON.parse(event.body);
             await collection.insertOne({ ...data, date: new Date() });
-            return { statusCode: 200, headers, body: "Saved" };
-        } else {
-            // Get all
-            const data = await collection.find({}).toArray();
-            return { statusCode: 200, headers, body: JSON.stringify(data) };
-        }
+            return { statusCode: 200, headers, body: JSON.stringify({ msg: "Saved" }) };
+        } 
+        
+        // GET Request
+        const data = await collection.find({}).toArray();
+        return { statusCode: 200, headers, body: JSON.stringify(data) };
+
     } catch (e) {
-        return { statusCode: 500, headers, body: e.toString() };
+        return { 
+            statusCode: 500, 
+            headers, 
+            body: JSON.stringify({ error: e.toString() }) 
+        };
     }
 };
